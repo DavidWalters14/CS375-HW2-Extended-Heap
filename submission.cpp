46538cad8bbe7aef31c77f92b5f2c30117ac85fb
David Walters <dwalter2@binghamton.edu>
2019-10-12 17:02:41 -0400

    working

--- submission.cpp
+++ submission.cpp
@@ -3,11 +3,16 @@
 #include <vector>
 #include <iostream>
 #include <string>
+#include <cstring>
+#include <string.h>
+#include <unordered_map>
+
 
 using namespace std;
 struct node{
 	int id;
 	int score;
+	bool eliminated = false;
 };
 
 int left(int index){
@@ -25,9 +30,10 @@ bool isLeaf(int index, vector<node> h){
 	if(left(index)>=h.size()){
 		return true;
 	}
+	//else if()
 	return false;
 };
-heapify(vector<node> &h, vector<int> &ha){
+void heapify(vector<node> &h, vector<int> &ha){
 	int i = 0;
 	if(h.size()%2==0){
 		i = (h.size()/2)-1;
@@ -41,7 +47,14 @@ heapify(vector<node> &h, vector<int> &ha){
 			if(n+1 < h.size() && h[n].score > h[n+1].score){
 				n = right(i);
 			}
-			if(h[i].score <= h[n].score){}
+			/*if(ha[i]==-1){
+				ha[i]=ha[n];
+			}*/
+			
+			if(h[i].score <= h[n].score){
+				//cout << "no op flag" << endl;
+				break;
+			}
 			else{
 				node temp = h[i];
 				h[i].id = h[n].id;
@@ -53,8 +66,17 @@ heapify(vector<node> &h, vector<int> &ha){
 				ha[n] = temp2;
 			}
 			i = n;
+			//cout << "i at end of loop" << i << endl;
 		}
 	}
+	for(int i = 1 ; i < h.size() ; i++){
+		ha[h[i].id]=i;
+	}
+	/*if(ha[1]==-1){
+		for(int i = 2 ; i < ha.size() ; i++){
+			ha[i]--;
+		}
+	}*/
 };
 
 int main(int argc, char** argv){
@@ -72,6 +94,7 @@ int main(int argc, char** argv){
 	input >> size;
 	vector<node> heap;
 	vector<int> handle;
+	unordered_map<int,int> mp;
 	node nullnode;
 	nullnode.id=NULL;
 	nullnode.score = NULL;
@@ -83,6 +106,7 @@ int main(int argc, char** argv){
 	}
 	string method;
 	while(input >> method){
+		//cout << method << endl;
 		if(method.compare("insertContestant")==0){
 			string id;
 			string score;
@@ -91,29 +115,42 @@ int main(int argc, char** argv){
 			int index1 = id.find('<');
 			int index2 = id.find('>');
 			int idnum = stoi(id.substr(index1+1,index2));
+			//cout << idnum << endl;
 			int index3 = score.find('<');
 			int index4 = score.find('>');
 			int scorenum = stoi(score.substr(index3+1,index4));
-			if(heap.size()+1==size){
+			//cout << scorenum << endl;
+			output << "insertContestant <" << idnum <<"> <" << scorenum << ">" << endl;
+			if(heap.size()>=size){
+				//cout << "if flag" << endl;
 				output << "Contestant <" << idnum << "> can not be inserted because the heap is full." << endl;
 			}
 			else if(handle[idnum]!=-1){
+				//cout << "else if flag" << endl;
 				output << "Contestant <" << idnum << "> is already in the extended heap: cannot insert." << endl;
 			}
 			else{
+				//cout << "else " << endl;
 				node temp;
 				temp.id = idnum;
 				temp.score = scorenum;
+				temp.eliminated = false;
 				heap.push_back(temp);
 				handle.resize(heap.size());
+				handle[idnum]=handle.size()-1;
+				
+				//cout << "handle size" << handle.size() << endl;
 				heapify(heap, handle);
+				//cout << "heapify flag" << endl;
 				output << "Contestant <" << idnum << "> inserted with initial score <" << scorenum << ">." << endl;
 			}
 		}
 		else if(method.compare("showContestants")==0){
+			output << "showContestants" << endl;
 			for(int i = 1 ; i < heap.size() ; i++){
+				//cout << "i: " << i << " handle[i] " << handle[i] << " heap score of i " << heap[i].score << endl;  
 				if(handle[i]!=-1){
-					output << "Contestant <" << i << "> in extended heap location <" << handle[i] << "> with score of <" << heap[handle[i]].score << ">." << endl;
+					output << "Contestant <" << heap[i].id << "> in extended heap location <" << handle[heap[i].id] << "> with score of <" << heap[i].score << ">." << endl;
 				}
 			}
 		}
@@ -123,6 +160,7 @@ int main(int argc, char** argv){
 			int index1 = id.find('<');
 			int index2 = id.find('>');
 			int idnum = stoi(id.substr(index1+1,index2));
+			output << "findContestant <" << idnum << ">" << endl;
 			if(handle[idnum]==-1){
 				output << "Contestant <" << idnum << "> is not in the extended heap." << endl;
 			}
@@ -131,15 +169,24 @@ int main(int argc, char** argv){
 			}
 		}
 		else if(method.compare("eliminateWeakest")==0){
+			output << "eliminateWeakest" << endl;
 			if(heap.size()==1){
 				output << "No contestant can be eliminated since the extended heap is empty." << endl;
 			}
 			else{
 				int id = heap[1].id;
 				int score = heap[1].score;
-				heap.erase(heap.begin()+1,heap.begin()+2);
+				//heap.erase(heap.begin()+1);
+				//handle.erase(handle.begin()+1,handle.begin()+2);
 				handle[id] = -1;
+				heap[1].eliminated = true;
+				heap[1] = heap[heap.size()-1];
+				handle[heap[1].id]=1;
+				handle[heap.size()-1]=-1;
+				heap.resize(heap.size()-1);
 				heapify(heap,handle);
+				handle[id]=-1;
+				mp[id]=score;
 				output << "Contestant <" << id << "> with current lowest score <" << score << ">. eliminated." << endl;
 			}
 		}
@@ -154,6 +201,8 @@ int main(int argc, char** argv){
 			int index3 = points.find('<');
 			int index4 = points.find('>');
 			int scorenum = stoi(points.substr(index3+1,index4));
+			output << "losePoints <" << idnum << "> <" <<scorenum <<">" <<endl;
+			//cout << "idnum : " << idnum << " handle[idnum]: " << handle[idnum] << " heap[handle[idnum]].id: " << heap[handle[idnum]].id << " heap[handle[idnum]].score: " << heap[handle[idnum]].score << endl;
 			if(handle[idnum]==-1){
 				output << "Contestant <" << idnum << "> is not in the extended heap." << endl;
 			}
@@ -174,6 +223,7 @@ int main(int argc, char** argv){
 			int index3 = points.find('<');
 			int index4 = points.find('>');
 			int scorenum = stoi(points.substr(index3+1,index4));
+			output << "earnPoints <" << idnum << "> <" <<scorenum <<">" <<endl;
 			if(handle[idnum]==-1){
 				output << "Contestant <" << idnum << "> is not in the extended heap." << endl;
 			}
@@ -184,46 +234,46 @@ int main(int argc, char** argv){
 			}
 		}
 		else if(method.compare("showHandles")==0){
-				for(int i = 1 ; i < heap.size() ; i++){
-					if(handle[i]==-1){
-						output << "There is no Contestant <" << i << "> in the extended heap: handle[<" << i <<">]=-1." << endl;
-					}
-					else{
-						output << "Contestant <" << i << "> stored in the extended heap location <" << handle[i] << ">." << endl;
-					}
+			output << "showHandles" << endl;
+			for(int i = 1 ; i < handle.size()+1 ; i++){
+				if(mp.find(i)!=mp.end()){
+					output << "There is no Contestant <" << i << "> in the extended heap: handle[<" << i <<">]=-1." << endl;
 				}
+				else if(i==handle.size()){
+					output << "There is no Contestant <" << i << "> in the extended heap: handle[<" << i <<">]=-1." << endl;
+				}
+				else{
+					output << "Contestant <" << i << "> stored in the extended heap location <" << handle[i] << ">." << endl;
+				}
+			}
 
 		}
 		else if(method.compare("crownWinner")==0){
+			output << "crownWinner" << endl;
+			heapify(heap, handle);
+			for(int i = heap.size() ; i >= 2 ; i--){
+				heap[1] = heap[heap.size()-1];
+				heap.resize(heap.size()-1);
 				heapify(heap, handle);
-				for(int i = heap.size() ; i >= 2 ; i--){
-					node temp = heap[i];
-					heap[i].id = heap[1].id;
-					heap[i].score = heap[1].score;
-					heap[1].id = temp.id;
-					heap[1].score = temp.score;
-					int temp2 = handle[i];
-					handle[i] = handle[1];
-					handle[1] = temp2;
-					vector<node> subheap(heap.begin(),heap.begin()+i-1);
-					heapify(subheap, handle);
-					heap = subheap;
-				}
+			}
+			output << "Contestant <" << heap[1].id << "> wins with score of <" << heap[1].score << ">!" << endl;
 		}
 		else if(method.compare("showLocation")==0){
-				string id;
-				input >> id;
-				int index1 = id.find('<');
-				int index2 = id.find('>');
-				int idnum = stoi(id.substr(index1+1,index2));
-				int index = handle[idnum];
-				if(index == -1){
-					output << "Contestant <" << idnum << "> stored in the extended heap location <" << handle[idnum] << ">." << endl;
-				}
-				else{
-					output << "Contestant <" << idnum << "> stored in the extended heap location <" << handle[idnum] << ">." << endl;
-				}
+			string id;
+			input >> id;
+			int index1 = id.find('<');
+			int index2 = id.find('>');
+			int idnum = stoi(id.substr(index1+1,index2));
+			int index = handle[idnum];
+			output << "showLocation <" << idnum << ">" << endl;
+			if(index == -1){
+				output << "Contestant <" << idnum << "> stored in the extended heap location <" << handle[idnum] << ">." << endl;
+			}
+			else{
+				output << "Contestant <" << idnum << "> stored in the extended heap location <" << handle[idnum] << ">." << endl;
+			}
 		}
+		//cout << method << endl;
 	}
 	input.close();
 	output.close();
